6c081b88c1ff2e37e0367ebd94b37d2073a72130
subham-codesigma <subham.ghorui@codesigma.tech>
2022-06-30 22:26:39 +0530

    services added

---
+++ app/Models/Service.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use DB;
+use Eloquent;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'small_description',
+        'display_image',
+        'banner_image',
+        'banner_image',
+        'footer_image'
+    ];
+
+    protected $primaryKey = 'id';
+    protected $table = 'services';
+
+    public function __construct()
+    {
+
+    }
+
+    static public function getListDataTable($order_column, $order_column_by, $limit, $offset, $search): array
+    {
+        $query = DB::table((new Service)->getTable());
+
+        if (!empty($search)) {
+            $query->where(function ($query) use ($search) {
+                $query->Where('title', 'like', '%' . $search . '%');
+            });
+        }
+
+        $query->select('*')->orderBy($order_column, $order_column_by);
+
+        $recordsTotal = $query->count();
+
+        $data = $query->skip($offset)->take($limit)->get()->toArray();
+
+        return ['recordsTotal' => $recordsTotal, 'data' => $data];
+    }
+}
