64a6a529b3e019bb4a1641de69742ba4808689fa
subham-codesigma <subham.ghorui@codesigma.tech>
2022-07-02 19:49:39 +0530

    editor added

--- resources/views/admin/service/add.blade.php
+++ resources/views/admin/service/add.blade.php
@@ -22,8 +22,14 @@
             </div>
 
             <div class="form-group">
-                <label class="control-label text-bold" for="description">Small Description <span class="required-fields">*</span></label>
-                <textarea class="form-control" rows="5" id="description" name="small_description" required data-msg-required="Please enter small description."></textarea>
+                <label class="control-label text-bold" for="small_description">Small Description <span class="required-fields">*</span></label>
+                <textarea class="form-control" rows="5" id="small_description" name="small_description" required data-msg-required="Please enter small description."></textarea>
+                <span class="validation-errors"></span>
+            </div>
+
+            <div class="form-group">
+                <label for="description" class="text-bold">Description <span class="required-fields">*</span></label>
+                <textarea class="form-control" name="description" id="description" rows="5" required data-msg-required="Please enter description."></textarea>
                 <span class="validation-errors"></span>
             </div>
 
@@ -50,10 +56,23 @@
     <script type="text/javascript" src="{{ URL::asset('assets/admin/js/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('assets/admin/js/jquery-validation/jquery.validate.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('assets/admin/js/jquery-validation/additional-methods.js') }}"></script>
+    <script src="//cdn.tiny.cloud/1/5lrxjaz6k4182tsdt4ftj6ezr145iiaejnw9nrannsilrejn/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
     <script type="text/javascript">
 
         $(document).ready(function () {
+            tinymce.init({
+                selector: 'textarea.form-control',
+                plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen link template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount textpattern noneditable help charmap quickbars emoticons',
+                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | template link anchor codesample | ltr rtl',
+                menubar: 'file edit view insert format tools table help',
+                toolbar_sticky: true,
+                toolbar_mode: 'sliding',
+                height: 300,
+                image_caption: true,
+                quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quicktable',
+                contextmenu: 'link table',
+            });
 
             $('#notice_form').validate({
                 ignore: [],
